3c9219f57ea3e6c20ffd9644c9fe5f10f58c768a
ChamikaRohan <chamika707@gmail.com>
2024-08-20 19:36:18 +0530

    Created basic forntend strucutre

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SocketClient from './components/SocketClient.jsx'
+
+export default function App() {
+  return (
+    <div>
+      <SocketClient/>
+    </div>
+  )
+}
