7ef287eb3ec49ae87dafcd22453588c5bc270c30
ChamikaRohan <chamika707@gmail.com>
2024-08-20 19:50:54 +0530

    Created socket-client connection and fixed cors

--- client/src/components/SocketClient.jsx
+++ client/src/components/SocketClient.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { io } from 'socket.io-client';
 
-export default function socketClient() {
+export default function SocketClient() {
+  const [socket, setSocket] = useState(null);
+
+  const connectSocket = () => {
+    if (!socket) {
+      const newSocket = io('http://localhost:8080');
+      setSocket(newSocket);
+
+      newSocket.on('connect', () => {
+        console.log('Connected to the server!');
+      });
+
+      newSocket.on('disconnect', () => {
+        console.log('Disconnected from the server.');
+      });
+    }
+  };
+
   return (
-    <div>socket-Client</div>
-  )
+    <div className="App">
+      <h1>Socket.IO Client</h1>
+      <button onClick={connectSocket}>Connect to Socket.IO Server</button>
+    </div>
+  );
 }
--- server/index.js
+++ server/index.js
@@ -2,12 +2,18 @@ import express from "express"
 import dotenv from "dotenv";
 import {Server} from "socket.io"
 import http from "http"
+import cors from "cors"
+
+const corsOptions = {
+    origin: process.env.CLIENT_URL, 
+    credentials: true 
+  };
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {cors: {corsOptions}});
 
 const PORT = process.env.PORT || 3000;
 
