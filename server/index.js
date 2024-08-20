5b642a448e1940f27c907ee24908bf2cbbd6bba1
ChamikaRohan <chamika707@gmail.com>
2024-08-20 19:14:13 +0530

    Created a server with socket.io

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import express from "express"
 import dotenv from "dotenv";
+import {Server} from "socket.io"
+import http from "http"
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+io.on('connection', (socket)=>{
+    console.log("A user is connected!");
+});
+
+server.listen(PORT, ()=>{
     return console.log(`Listening from port: ${PORT}`)
 })
