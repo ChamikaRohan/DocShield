f20520ba2c24086813756bacacd467e2d0edfaa6
SandunikaThanthriwatta <s.thathriwatta07@gmail.com>
2024-10-08 22:38:45 +0530

    Updated the frontend to be responsive

--- client/src/pages/Inbox.jsx
+++ client/src/pages/Inbox.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useEffect, useState } from 'react'; 
+import Navbar from '../components/Navbar';
 import DocCard from '../components/DocCard';
+import './inbox.css';
 
 const Inbox = () => {
+  const [columns, setColumns] = useState('repeat(4, 1fr)'); 
+
+  const updateColumns = () => {
+    const width = window.innerWidth;
+    if (width < 600) {
+      setColumns('repeat(1, 1fr)'); 
+    } else if (width < 768) {
+      setColumns('repeat(2, 1fr)'); 
+    } else if (width < 1024) {
+      setColumns('repeat(3, 1fr)'); 
+    } else {
+      setColumns('repeat(4, 1fr)'); 
+    }
+  };
+
+  useEffect(() => {
+    updateColumns(); 
+    window.addEventListener('resize', updateColumns); 
+
+    return () => {
+      window.removeEventListener('resize', updateColumns); 
+    };
+  }, []);
+
   return (
     <div>
-      <Navbar/>
-        <div style={{ padding: '80px',justifyContent: 'center',mx:'auto',  margin: '5px',marginBottom: '25px', columnGap: '0px', marginTop: '40px', display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '45px',justifyItems: 'center',my:'auto' }}>
-            {[...Array(8)].map((_, index) => (
-              <DocCard key={index} />
-            ))}
-        </div>
+      <Navbar />
+      <div
+        style={{
+          padding: '80px',
+          margin: '5px',
+          marginBottom: '25px',
+          marginTop: '40px',
+          display: 'grid',
+          gridTemplateColumns: columns, 
+          gap: '45px',
+          justifyItems: 'center',
+          mx: 'auto',
+          my: 'auto',
+        }}
+      >
+        {[...Array(8)].map((_, index) => (
+          <DocCard key={index} />
+        ))}
+      </div>
     </div>
-    
-  )
-}
+  );
+};
 
 export default Inbox;
