6e314e4d6e7e3655657a8c89bda97fd30da9f3d7
ChamikaRohan <chamika707@gmail.com>
2024-10-18 20:24:16 +0530

    Created entry point UI route for signin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import CheckAuth from './components/CheckAuth.jsx';
 import SocketClient from './components/SocketClient.jsx';
 import ViewMyDocs from './components/ViewMyDocs.jsx';
 import Inbox from './pages/Inbox.jsx';
 import Compose from './pages/Compose.jsx';
 import Login from './pages/Login.jsx';
-import './App.css'
+import './App.css';
 import ForgotPasswordPage from './pages/forgot-password.jsx';
 import SignUpPage from './pages/Signup.jsx';
 
 export default function App() {
   return (
-
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* Redirect from "/" to "/signin" */}
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<Login />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/compose" element={<Compose />} /> 
         <Route path="/inbox" element={<Inbox />} />
-
         <Route path="/docs" element={<ViewMyDocs />} />
         <Route path="/socket" element={<SocketClient />} />
         <Route path="/forgotpw" element={<ForgotPasswordPage />} />
-
       </Routes>
     </Router>
   );
